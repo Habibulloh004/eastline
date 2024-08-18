2545110dbd1cd6d0f5cd5d28e17573dcc06deeb6
habibulloh004 <habibulloh4080@gmail.com>
2024-08-18 13:45:47 +0500

    newnew

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -4,28 +4,29 @@ import Banner from "@/components/shared/banner";
 import { getLastProducts } from "@/lib/utils";
 
 export default async function Home() {
-  const products = await db.product.findMany();
-  const categories = await db.category.findMany({
-    include: {
-      products: true,
-    },
-  });
-  const topCategories = await db.topCategory.findMany();
+  // const products = await db.product.findMany();
+  // const categories = await db.category.findMany({
+  //   include: {
+  //     products: true,
+  //   },
+  // });
+  // const topCategories = await db.topCategory.findMany();
 
-  function getRandomProducts(products, count = 4) {
-    const shuffled = products.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, count);
-  }
+  // function getRandomProducts(products, count = 4) {
+  //   const shuffled = products.sort(() => 0.5 - Math.random());
+  //   return shuffled.slice(0, count);
+  // }
 
-  const randomProducts = getRandomProducts(products);
+  // const randomProducts = getRandomProducts(products);
 
-  const lastProducts = getLastProducts(products);
+  // const lastProducts = getLastProducts(products);
 
   return (
     <div className="min-h-[50%] py-10 flex flex-col items-center justify-center">
-      <Banner products={randomProducts} categories={categories} />
+      hello
+      {/* <Banner products={randomProducts} categories={categories} />
       <AllCategories categories={categories} topCategories={topCategories} />
-      <AllProducts products={lastProducts} categories={categories} />
+      <AllProducts products={lastProducts} categories={categories} /> */}
     </div>
   );
 }
